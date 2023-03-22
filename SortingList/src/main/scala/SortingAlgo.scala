0dece7d15d92063be838f2fb5ef0c80b2206ef49
Sant <sant.singh@knoldus.com>
2023-03-22 23:30:10 +0530

    adding sorting method

--- SortingList/src/main/scala/SortingAlgo.scala
+++ SortingList/src/main/scala/SortingAlgo.scala
@@ -1,23 +1,36 @@
 package com.knoldus.sortinglist
 
 case class BubbleSort() {
-  def getLargest(data: List[Int]): (Int, List[Int]) =
-    data match {
-      case head :: Nil => (head, Nil)
-      case head :: tail =>
-        val (large, remaining) = getLargest(tail)
-        if (large > head)
-          (large, head :: remaining)
-        else
-          (head, large :: remaining)
-    }
 
-  def bubbleSort(data: List[Int]): List[Int] =
+  // bubble sort
+  def bubbleSort(data: List[Int]): List[Int] = {
+    def getLargest(data: List[Int]): (Int, List[Int]) =
+      data match {
+        case head :: Nil => (head, Nil)
+        case head :: tail =>
+          val (large, remaining) = getLargest(tail)
+          if (large > head)
+            (large, head :: remaining)
+          else
+            (head, large :: remaining)
+      }
     data match {
       case Nil => Nil
       case _ =>
         val (greatest, tail) = getLargest(data)
         bubbleSort(tail) ::: List(greatest)
     }
+  }
+
+  //insertion sort
+  def insertionSort(list: List[Int]): List[Int] = {
+    def insert(number: Int, sortedList: List[Int]): List[Int] =
+      if(sortedList.isEmpty || number <= sortedList.head) number :: sortedList
+      else sortedList.head :: insert(number, sortedList.tail)
+
+    if (list.isEmpty || list.tail.isEmpty) list
+    else insert(list.head, insertionSort(list.tail))
+  }
+
 }
 
--- SortingList/src/test/scala/SprtingAlgoTest.scala
+++ SortingList/src/test/scala/SprtingAlgoTest.scala
@@ -3,14 +3,15 @@ package com.knoldus.sortinglist
 import org.scalatest.funsuite.AnyFunSuite
 
 class SprtingAlgoTest extends AnyFunSuite {
-  test("Test1") {
-    val list1 = List(1, 5, 3, 2)
-    assert(BubbleSort().bubbleSort(list1) === List(1, 2, 3, 5))
+
+  val list: List[Int] = List(1, 5, 3, 2)
+
+  test("bubble sort") {
+    assert(BubbleSort().bubbleSort(list) === List(1, 2, 3, 5))
   }
 
-  test("Test3") {
-    val list3 = List(4, 2, 3)
-    assert(BubbleSort().bubbleSort(list3) != List(4, 2, 3))
+  test("insertion sort") {
+    assert(BubbleSort().insertionSort(list) === List(1, 2, 3, 5))
   }
 
 }
